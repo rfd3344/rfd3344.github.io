8a1ed5761705c7e3984c5d37f36f46a816f7cad1
eric <rfd3344w@gmail.com>
2023-07-02 20:13:01 +1000

    Update gitStatic.js

--- src/api/gitStatic.js
+++ src/api/gitStatic.js
@@ -9,14 +9,14 @@ import {
 import { masterMock } from './gitStaticMock';
 
 export const getMasterTree = async () => {
-  return masterMock;
+  // return masterMock;
   const resp = await gitStaticApi().get('trees/master?recursive=1');
   return resp;
 };
 
 
-export const getResource = async (url = '') => {
-  return {};
-  const resp = await createInstance({ baseURL: url }).get();
-  return resp;
-};
+// export const getResource = async (url = '') => {
+//   return {};
+//   const resp = await createInstance({ baseURL: url }).get();
+//   return resp;
+// };
