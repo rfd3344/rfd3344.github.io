c235db93ad2f1f502973c5224312b8b3f0db4408
eric <rfd3344w@gmail.com>
2023-07-07 17:28:21 +1000

    1

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -3,10 +3,12 @@ import _ from 'lodash';
 import { FileTypeEnum } from 'src/constants/fileConst';
 
 export const getFileTyle = (fileName = '') => {
-  const fileExtension = fileName.split('.').pop();
+  const fileExtension = _.lowerCase(fileName.split('.').pop());
 
   if (fileExtension === 'pdf') {
     return FileTypeEnum.pdf;
+  } else if (fileExtension === 'md') {
+    return FileTypeEnum.md;
   } else if (['jpg', 'jpeg', 'png'].includes(fileExtension)) {
     return FileTypeEnum.image;
   }
