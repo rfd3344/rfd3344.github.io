65b070eebd665be8ad32555ab5de84923489251a
eric <rfd3344w@gmail.com>
2023-07-12 18:50:17 +1000

    1

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,7 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-// import logger from 'redux-logger';
 
-// import { isDev } from 'src/utils/devUtils';
+
 import core from './coreSlice';
 // GitHub/rfd3344.github.io/src/pages/staticRepo/staticRepoSlice.jsx
 import staticRepo from 'src/pages/staticRepo/staticRepoSlice';
