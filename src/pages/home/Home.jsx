6a2bef462ba5cc93bdc13566510d55851cc16af9
eric <rfd3344w@gmail.com>
2023-07-13 18:15:44 +1000

    1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -79,12 +79,15 @@ export default function Home() {
         <LinkButton text='dbdiagram' to='https://dbdiagram.io/d/5e2790869e76504e0ef083ac' />
         <LinkButton text='toolfk:devTools' to='https://www.toolfk.com' icon={<LinkIcon />} />
         <LinkButton text="算法总结" to='https://github.com/youngyangyang04/leetcode-master/tree/master/problems' />
+        <LinkButton to='https://digitalcloud.training/aws-cheat-sheets/' text='aws-cheat-sheets' />
+
       </Section>
 
 
       <Section title="Others">
+
         <LinkButton to={PagePath.test} text='test' color="secondary" />
-        <LinkButton to={PagePath.webpage} text='webpage' color="secondary" />
+        {/* <LinkButton to={PagePath.webpage} text='webpage' color="secondary" /> */}
       </Section>
 
     </Container >
