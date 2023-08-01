11b05d36ed9556cfb01e34ec0b2ae1ae2bd12640
eric <eric.li@carly.co>
2023-08-01 12:14:42 +1000

    1

--- src/pages/home/homeConst.js
+++ src/pages/home/homeConst.js
@@ -3,48 +3,49 @@ import { PagePath } from 'src/constants/routerConst';
 
 
 export const ToolsLinks = {
-  'Calculator': PagePath.calculator,
-  'ColorTable': PagePath.colorTable,
-  'webpage': PagePath.webpage,
-  'PhotoshopOnline': 'https://photoshop.adobe.com',
-  'FileConvert': 'https://photoshop.adobe.com',
-  'payCalculator': 'https://paycalculator.com.au',
+  Calculator: PagePath.calculator,
+  ColorTable: PagePath.colorTable,
+  Wwebpage: PagePath.webpage,
+  PhotoshopOnline: 'https://photoshop.adobe.com',
+  FileConvert: 'https://photoshop.adobe.com',
+  PayCalculator: 'https://paycalculator.com.au',
 };
 
 export const CloudLinks = {
-  'GoogleKeep': 'https://keep.google.com',
-  'DriveNotes': 'https://drive.google.com/drive/folders/1XA8VemDb4wW-kstLImPOuOEW9Qi6g02J',
-  'Skype': 'https://web.skype.com',
+  GoogleKeep: 'https://keep.google.com',
+  DriveNotes: 'https://drive.google.com/drive/folders/1XA8VemDb4wW-kstLImPOuOEW9Qi6g02J',
+  Skype: 'https://web.skype.com',
 };
 
 export const GithubLinks = {
-  'RepoFiles': PagePath.repo,
-  'PageCode': 'https://github.com/rfd3344/rfd3344.github.io',
-  'staticJH': 'https://github.com/rfd3344/staticJH',
-  'Storybook': 'https://rfd3344w.github.io',
+  RepoFiles: PagePath.repo,
+  PageCode: 'https://github.com/rfd3344/rfd3344.github.io',
+  StaticJH: 'https://github.com/rfd3344/staticJH',
+  Storybook: 'https://rfd3344w.github.io',
 };
 
 export const WebLinks = {
-  'ChatGPT': 'https://chat.openai.com',
-  'dbdiagram': 'https://dbdiagram.io/d/5e2790869e76504e0ef083ac',
-  'digitalcloudAWS': 'https://digitalcloud.training/aws-cheat-sheets',
-  'codingnconceptsAWS': 'https://codingnconcepts.com/aws/aws-certified-solutions-architect-associate',
+  ChatGPT: 'https://chat.openai.com',
+  dbdiagram: 'https://dbdiagram.io/d/5e2790869e76504e0ef083ac',
+  DigitalCloud: 'https://digitalcloud.training/aws-cheat-sheets',
+  CodingNConcepts: 'https://codingnconcepts.com/aws/aws-certified-solutions-architect-associate',
   'toolfk:devTools': 'https://www.toolfk.com',
   '算法总结': 'https://github.com/youngyangyang04/leetcode-master/tree/master/problems',
 };
 
 export const WorkLinks = {
-  'WDoc': 'https://docs.google.com/document/d/1zXNje7E74rW9ICdVE4JMvw6e_6jHfdEsVCcqw8TzJnk/edit',
+  WDoc: 'https://docs.google.com/document/d/1zXNje7E74rW9ICdVE4JMvw6e_6jHfdEsVCcqw8TzJnk/edit',
+  Console: 'https://ap-southeast-2.console.aws.amazon.com/console',
 
 };
 
 export const GameLinks = {
-  'Genshin': 'https://wiki.biligame.com/ys',
-  'StarRail': 'https://wiki.biligame.com/sr',
+  Genshin: 'https://wiki.biligame.com/ys',
+  StarRail: 'https://wiki.biligame.com/sr',
   '爱壹帆': 'https://www.yfsp.tv',
-  'Bilibili': 'https://www.bilibili.com',
+  Bilibili: 'https://www.bilibili.com',
 };
 
 export const OtherLinks = {
-  'test': PagePath.test,
+  Test: PagePath.test,
 };
--- src/pages/home/ui/LinkButton.jsx
+++ src/pages/home/ui/LinkButton.jsx
@@ -48,8 +48,9 @@ export default function LinkButton({
   const startIcon = (() => {
     if (_.includes(to, 'keep.google.com')) return <NoteIcon />;
     if (_.includes(to, 'drive.google.com')) return <DriveIcon />;
-    if (_.includes(to, 'skype.com')) return <ImgIcon src="./icons/skype.svg" />;
     if (_.includes(to, 'github.com')) return <GitHubIcon />;
+    if (_.includes(to, 'aws')) return <ImgIcon src="./icons/aws.png" />;
+    if (_.includes(to, 'skype.com')) return <ImgIcon src="./icons/skype.svg" />;
     if (_.includes(to, 'rfd3344w.github.io')) return <ImgIcon src="./icons/storybook.png" />;
     if (linkType === LinkTypes.external) return <OpenInNewIcon />;
     if (to === PagePath.calculator) return <CalculateIcon />;
