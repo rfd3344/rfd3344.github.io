f2b18e5153e74dd69377b9b9cfe8e84c8bf55d1d
eric <eli@cardiex.com>
2025-08-10 14:28:22 +1000

    1

--- docs/index.js
+++ docs/index.js
@@ -1,45 +1,176 @@
 
 
-console.warn('index.js loaded 333');
 
-const categories = [
+const homeLinks = [
   {
-    title: "Fruits",
-    items: ["Apple", "Banana", "Orange"]
+    categoryTitle: 'Notes & Files',
+    links: [
+      { text: 'LocalNotes', url: '/html/localNotes.html?notes=2' },
+      { text: 'DailyNote', url: 'https://rfd3344.atlassian.net/wiki/spaces/estelspace/pages/edit-v2/20480026' },
+      { text: 'DriveDocs', url: 'https://drive.google.com/drive/folders/1XA8VemDb4wW-kstLImPOuOEW9Qi6g02J' },
+      { text: 'slack', url: 'https://app.slack.com/client/T07BG5VAJTS/C08Q2LDL7BR' },
+      { text: 'teams', url: 'https://teams.live.com/v2' },
+      { text: 'StaticFiles', url: '/github-repo' },
+      { text: 'GithubRepo', url: 'https://github.com/rfd3344?tab=repositories' },
+    ],
   },
   {
-    title: "Vegetables",
-    items: ["Carrot", "Broccoli", "Spinach"]
+    categoryTitle: 'Tools',
+    links: [
+      { text: 'Calculator', url: '/html/calculator.html' },
+      { text: 'ChatGPT', url: 'https://chat.openai.com' },
+      { text: 'DeepSeek', url: 'https://chat.deepseek.com/' },
+      { text: 'PayCalculator', url: 'https://paycalculator.com.au' },
+      { text: 'ILovePdf', url: 'https://www.ilovepdf.com' },
+      { text: 'tinyTools', url: 'https://tinywow.com/' },
+      { text: 'snap-drop', url: 'https://www.snap-drop.net/' },
+      { text: 'skylinewebcams', url: 'https://www.skylinewebcams.com/en.html' },
+      { text: 'Demanualslibmo', url: 'https://www.manualslib.com/' },
+    ],
   },
   {
-    title: "Drinks",
-    items: ["Water", "Juice", "Tea"]
-  }
+    categoryTitle: 'Image',
+    links: [
+      { text: 'ColorTable', url: '/html/colorTable.html' },
+      { text: 'PhotoPea', url: 'https://photopea.com' },
+      { text: 'ImageOnline', url: 'https://www.imgonline.com.ua/eng' },
+      { text: 'ExcaliDraw', url: 'https://excalidraw.com/' },
+
+      { text: 'Pixabay', url: 'https://pixabay.com' },
+      { text: 'Unsplash', url: 'https://unsplash.com' },
+      { text: 'Pexels', url: 'https://www.pexels.com' },
+    ],
+  },
+
+  {
+    categoryTitle: 'Tech',
+    links: [
+      { text: 'APIGenerator', url: 'https://retool.com/api-generator' },
+      { text: 'DBDiagram', url: 'https://dbdiagram.io/d/demo-6724a1c7b1b39dd8582997d4' },
+      { text: 'AWSCheatSheets', url: 'https://digitalcloud.training/aws-cheat-sheets' },
+      { text: 'CodingNConcepts', url: 'https://codingnconcepts.com/aws/aws-certified-solutions-architect-associate' },
+      { text: 'ToolFKDevTools', url: 'https://www.toolfk.com' },
+      { text: '算法总结', url: 'https://github.com/youngyangyang04/leetcode-master/tree/master/problems' },
+      { text: 'LeetCode', url: 'https://leetcode.com/problemset/all/' },
+      { text: 'ReactNativeSample', url: 'https://github.com/ReactNativeNews/React-Native-Apps' },
+    ],
+  },
+  {
+    categoryTitle: 'Work',
+    links: [
+      // { text: 'PostmanJH', url: 'https://rfd3344.postman.co/workspace/JH~57a4fc2b-3943-4488-a483-ba3b6d21e6cd/request/10631940-8df98a7f-bdd9-4e8f-a9b4-b9715a86a108' },
+      // { text: 'Figma', url: 'https://www.figma.com/files/' },
+      { text: 'SeekReact', url: 'https://www.seek.com.au/react-jobs/in-Sydney-NSW-2000' },
+      { text: 'WNote', url: 'https://rfd3344.atlassian.net/wiki/spaces/rfd3344/pages/edit-v2/18055186' },
+
+      { text: 'GoogleDrive', url: 'https://drive.google.com/drive/folders/1SPa-puKlt5b8IE8JcvkCiwv8u2WK6F8M' },
+      { text: 'Jira', url: 'https://cardiex.atlassian.net/jira/software/c/projects/SR/boards/27/backlog?assignee=712020%3A44d493fb-32ce-4106-808e-5cc38ee4746f' },
+
+      { text: 'Confluence', url: 'https://cardiex.atlassian.net/wiki/spaces/NB/overview' },
+      { text: 'Github', url: 'https://github.com/orgs/CardieX/repositories?type=all' },
+      { text: 'AWSPortal', url: 'https://d-9067cf9945.awsapps.com/start/#/?tab=accounts' },
+    ],
+  },
+  {
+    categoryTitle: 'Bill',
+    links: [
+      { text: 'CleanUpCollection', url: 'https://www.thehills.nsw.gov.au/Services/For-Residents/Waste-Recycling/Clean-Up-Collection' },
+      { text: 'SydneyWater', url: 'https://myaccount.sydneywater.com.au/Portal/Dashboard.aspx' },
+      { text: 'HillsCouncilInvoice', url: 'https://thehills.enotices.com.au/login?email=rfd3344@gmail.com' },
+      { text: 'DEFTLevy', url: 'https://personal.deft.com.au/#/payer/dashboard/transactions' },
+      { text: 'EToll', url: 'https://account.myetoll.transport.nsw.gov.au/login' },
+      { text: 'NRMA', url: 'https://connect.nrma.com.au/welcome/login/username' },
+      { text: 'ServiceNSW', url: 'https://account.service.nsw.gov.au/overview' },
+      { text: 'ColesMobile', url: 'https://colesmobile.com.au/pages/dashboard/service/84870' },
+
+    ],
+  },
+  {
+    categoryTitle: 'Fun',
+    links: [
+      { text: 'GenShin', url: 'https://wiki.biligame.com/ys' },
+      { text: 'StarRail', url: 'https://wiki.biligame.com/sr' },
+      { text: '虎牙直播', url: 'https://www.huya.com/g/wzry' },
+      { text: '爱壹帆', url: 'https://www.google.com/search?q=yfsp' },
+      { text: 'BiLiBiLi', url: 'https://www.bilibili.com' },
+      { text: 'Poki', url: 'https://poki.com/' },
+      { text: '红警2', url: 'https://ra2web.com' },
+      { text: 'CS-Online', url: ' https://play-cs.com/en/servers' },
+    ],
+  },
+  {
+    categoryTitle: 'Sport',
+    links: [
+      { text: 'PerisherSki', url: 'https://www.epicaustraliapass.com.au/' },
+      { text: 'TennisHill', url: 'https://hills.bookable.net.au/search?categoryId=4' },
+      { text: 'CastleHillTennis', url: 'https://www.tennisvenues.com.au/booking/hdta-caterson-tc' },
+      { text: 'TennisParramatta', url: 'https://www.cityofparramatta.nsw.gov.au/recreation/tennis-courts' },
+      { text: 'TennisAustralia', url: 'https://play.tennis.com.au/search?location=KELLYVILLE,%20NSW%202135&dateDisplay=Today&time=1230&orderBy=DISTANCE&activities=RESOURCE&quickbook=true' },
+      { text: 'HillsLeague', url: 'https://my.gloryleague.basketball/games' },
+    ],
+  },
+  {
+    categoryTitle: 'Shopping',
+    links: [
+      { text: 'StockX', url: 'https://stockx.com/' },
+      { text: 'OzBargain', url: 'https://www.ozbargain.com.au/' },
+      { text: '澳洲省钱快报', url: 'https://www.dealmoon.com.au/' },
+      { text: 'Temu', url: 'https://www.temu.com/au' },
+      { text: 'Shein', url: 'https://au.shein.com/' },
+      { text: 'MSY', url: 'https://www.msy.com.au/' },
+      { text: 'MVave', url: 'https://www.mwave.com.au/' },
+      { text: 'FlowerBloomex', url: 'https://bloomex.com.au/' },
+      { text: 'FlowerWynyard', url: 'https://www.cbdflowerswynyard.com.au/' },
+      { text: 'FlowerForEveryone', url: 'https://www.flowersforeveryone.com.au/' },
+      { text: 'GiftDoll', url: 'https://omgstation.com.au/' },
+      { text: 'Groupon', url: 'https://www.groupon.com.au/' },
+    ],
+  },
+  {
+    categoryTitle: 'Health',
+    links: [
+      { text: 'InstantScript', url: 'https://www.instantscripts.com.au/' },
+    ],
+  },
+  {
+    categoryTitle: 'Others',
+    links: [
+      { text: 'Demo', url: '/demo' },
+
+      { text: 'Demo', url: '/demo' },
+    ],
+  },
 ];
 
-const container = document.getElementById("container");
 
-categories.forEach(category => {
-  const categoryDiv = document.createElement("div");
-  categoryDiv.className = "category";
 
-  const titleEl = document.createElement("div");
-  titleEl.className = "category-title";
-  titleEl.textContent = category.title;
-  categoryDiv.appendChild(titleEl);
 
-  const buttonList = document.createElement("div");
-  buttonList.className = "button-list";
+const renderCategoryList = (categoryList) => {
+  const container = document.getElementById("container");
 
-  category.items.forEach(item => {
-    const btn = document.createElement("button");
-    btn.textContent = item;
-    btn.addEventListener("click", () => {
-      alert(`You clicked ${item}`);
+  categoryList.forEach(category => {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.className = "category";
+
+    const titleEl = document.createElement("div");
+    titleEl.className = "category-title";
+    titleEl.textContent = category.categoryTitle;
+    categoryDiv.appendChild(titleEl);
+
+    const buttonList = document.createElement("div");
+    buttonList.className = "button-list";
+
+    category.links.forEach(item => {
+      const link = document.createElement("a");
+      link.href = item.url;
+      link.textContent = item.text;
+      // link.target = "_blank"; // Open in new tab
+      buttonList.appendChild(link);
     });
-    buttonList.appendChild(btn);
-  });
 
-  categoryDiv.appendChild(buttonList);
-  container.appendChild(categoryDiv);
-});
+    categoryDiv.appendChild(buttonList);
+    container.appendChild(categoryDiv);
+  });
+};
+
+renderCategoryList(homeLinks);
