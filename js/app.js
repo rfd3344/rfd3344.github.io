6dd213d5fb1c527a1c575c49c0388b76cd41b7a4
eric <eli@conneqthealth.com>
2026-04-17 20:18:55 +1000

    1

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+function checkOnlineStatus() {
+  if (navigator.onLine) console.log('internet-status: ONLINE ✅');
+  else console.log('internet-status: OFFLINE 🚫 ');
+}
+
+window.addEventListener('online', checkOnlineStatus);
+window.addEventListener('offline', checkOnlineStatus);
+checkOnlineStatus();
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js')
+    .then(() => console.log('Service Worker Registered'));
+}
