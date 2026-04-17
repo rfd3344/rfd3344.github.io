852fe080b4b11c02a90460f0ed28b2e6955fa745
eric <eli@conneqthealth.com>
2026-04-17 20:43:42 +1000

    v2.9

--- js/offline-service-worker.js
+++ js/offline-service-worker.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = 'offline-demo-v1';
 
 const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './js/offline-available.js',
-  './js/category-links.js',
-  './pages/local-notes.html',
-  './pages/calendar.html',
+  '/',
+  '/index.html',
+  '/js/offline-available.js',
+  '/js/category-links.js',
+  '/pages/local-notes.html',
+  '/pages/calendar.html',
 
-  './manifest.json',
-  './assets/favicon.ico',
-  './assets/icon-192.jpg'
+  '/manifest.json',
+  '/assets/favicon.ico',
+  '/assets/icon-192.jpg'
 ];
 
 // Install → cache files
